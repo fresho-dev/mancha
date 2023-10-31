50ba53c9163f47d240dcf31b1a1c97dbc767d2b4
oscar <oscar@wahltinez.org>
2023-10-31 15:10:28 +1100

    Allow preprocessing of head element

--- dist/browser.js
+++ dist/browser.js
@@ -1,13 +1,25 @@
 "use strict";
-var _a, _b;
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var _a, _b, _c, _d;
 Object.defineProperty(exports, "__esModule", { value: true });
 const Mancha = require("./web.js");
 self["Mancha"] = Mancha;
 if (((_b = (_a = self.document) === null || _a === void 0 ? void 0 : _a.currentScript) === null || _b === void 0 ? void 0 : _b.getAttribute("init")) !== undefined) {
     const vars = JSON.parse(self.document.currentScript.dataset["vars"] || "{}");
     const fsroot = self.location.href.split("/").slice(0, -1).join("/") + "/";
-    Mancha.renderContent(self.document.body.innerHTML, vars, fsroot).then((content) => {
-        self.document.body.innerHTML = content;
-    });
+    const targets = ((_d = (_c = self.document.currentScript) === null || _c === void 0 ? void 0 : _c.getAttribute("target")) === null || _d === void 0 ? void 0 : _d.split(",")) || ["body"];
+    targets.forEach((target) => __awaiter(void 0, void 0, void 0, function* () {
+        console.log(`Replacing target ${target}.`);
+        const node = self.document[target];
+        node.innerHTML = yield Mancha.renderContent(node.innerHTML, vars, fsroot);
+    }));
 }
 exports.default = Mancha;
--- src/browser.ts
+++ src/browser.ts
@@ -5,8 +5,10 @@ import * as Mancha from "./web.js";
 if (self.document?.currentScript?.getAttribute("init") !== undefined) {
   const vars = JSON.parse(self.document.currentScript.dataset["vars"] || "{}");
   const fsroot = self.location.href.split("/").slice(0, -1).join("/") + "/";
-  Mancha.renderContent(self.document.body.innerHTML, vars, fsroot).then((content) => {
-    self.document.body.innerHTML = content;
+  const targets = self.document.currentScript?.getAttribute("target")?.split(",") || ["body"];
+  targets.forEach(async (target) => {
+    const node = (self.document as any)[target] as Element;
+    node.innerHTML = await Mancha.renderContent(node.innerHTML, vars, fsroot);
   });
 }
 
