5cc5088f44a431a17b3876443c4f7693bf6fd959
oscar <oscar@wahltinez.org>
2023-12-19 21:17:17 -0500

    Update examples

--- examples/wrangler/index.js
+++ examples/wrangler/index.js
@@ -1,9 +1,9 @@
-import { WorkerRouter } from "@worker-tools/router";
+import { Router } from "itty-router";
 import * as Mancha from "mancha/dist/web";
 
 const HTML_ROOT = "https://fresho-dev.github.io/mancha/examples/rendered/html";
 
-const router = new WorkerRouter();
+const router = Router();
 
 router.get("/", async (req) => {
   const params = new URL(req.url).searchParams;
@@ -12,4 +12,6 @@ router.get("/", async (req) => {
   return new Response(html, { headers: { "Content-Type": "text/html" } });
 });
 
-export default router;
+export default {
+  fetch: (req, env, ctx) => router.handle(req, env, ctx),
+};
