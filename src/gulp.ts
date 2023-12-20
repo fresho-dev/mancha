5899db4119f72ac72410feb9334f89702a0cd530
oscar <oscar@wahltinez.org>
2023-12-19 22:25:26 -0500

    Update examples

--- src/gulp.ts
+++ src/gulp.ts
@@ -8,7 +8,7 @@ import * as Mancha from "./index.js";
 /**
  * Main entrypoint to be used in Gulp. Usage:
  *
- *     var mancha = require('gulp-mancha')
+ *     var mancha = require('mancha/dist/gulp')
  *     gulp.src(...).pipe(mancha({myvar: myval})).pipe(...)
  *
  * @param vars <key, value> pairs of literal string replacements. `key` will become `{{key}}` before
