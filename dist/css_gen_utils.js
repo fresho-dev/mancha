f8e6345796bf8275ae9373c12cad78b643de5bca
oscar <oscar@wahltinez.org>
2024-09-30 21:31:58 +1000

    Add CSS shadows

--- dist/css_gen_utils.js
+++ dist/css_gen_utils.js
@@ -146,6 +146,16 @@ const PROPS_CUSTOM = {
     "rounded-lg": { "border-radius": ".5rem" },
     "rounded-xl": { "border-radius": ".75rem" },
     "rounded-full": { "border-radius": "9999px" },
+    // Shadows.
+    shadow: { "box-shadow": "0 0 1px 0 rgba(0, 0, 0, 0.05)" },
+    "shadow-sm": { "box-shadow": "0 1px 2px 0 rgba(0, 0, 0, 0.05)" },
+    "shadow-md": { "box-shadow": "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)" },
+    "shadow-lg": { "box-shadow": "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)" },
+    "shadow-xl": { "box-shadow": "0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)" },
+    "shadow-2xl": { "box-shadow": "0 25px 50px -12px rgba(0, 0, 0, 0.25)" },
+    "shadow-inner": { "box-shadow": "inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)" },
+    "shadow-outline": { "box-shadow": "0 0 0 3px rgba(66, 153, 225, 0.5)" },
+    "shadow-none": { "box-shadow": "none" },
     // Transitions.
     "transition-none": { transition: "none" },
     transition: { transition: `all 150ms ease-in-out` },
