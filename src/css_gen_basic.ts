757022cafbbdd52a4f772be9fe532a4a72a7d5b9
Oscar Wahltinez <41451293+owahltinez@users.noreply.github.com>
2025-01-06 05:34:20 +0000

    Remove newlines from minimized output

--- src/css_gen_basic.ts
+++ src/css_gen_basic.ts
@@ -1,23 +1,22 @@
-import { SafeStyleSheet, safeStyleSheet } from "safevalues";
+import { SafeStyleSheet, concatStyleSheets, safeStyleSheet } from "safevalues";
 
 export default function rules(): SafeStyleSheet {
   // Based on https://www.swyx.io/css-100-bytes.
-  return safeStyleSheet`
-    html {
-        max-width: 70ch;
-        padding: 2em 1em;
-        margin: auto;
-        line-height: 1.75;
-        font-size: 1.25em;
-        font-family: sans-serif;
-    }
-
-    h1,h2,h3,h4,h5,h6 {
-        margin: 1em 0 0.5em;
-    }
-
-    p,ul,ol {
-        margin-bottom: 1em;
-        color: #1d1d1d;
-    }`;
+  return concatStyleSheets([
+    safeStyleSheet`html{`,
+    safeStyleSheet`max-width: 70ch;`,
+    safeStyleSheet`padding: 2em 1em;`,
+    safeStyleSheet`margin: auto;`,
+    safeStyleSheet`line-height: 1.75;`,
+    safeStyleSheet`font-size: 1.25em;`,
+    safeStyleSheet`font-family: sans-serif;`,
+    safeStyleSheet`}`,
+    safeStyleSheet`h1,h2,h3,h4,h5,h6{`,
+    safeStyleSheet`margin: 1em 0 0.5em;`,
+    safeStyleSheet`}`,
+    safeStyleSheet`p,ul,ol{`,
+    safeStyleSheet`margin-bottom: 1em;`,
+    safeStyleSheet`color: #1d1d1d;`,
+    safeStyleSheet`}`,
+    ]);
 }
