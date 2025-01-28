d0de0f1f2639ef5b42a13bcb943a095282a9ece2
Oscar Wahltinez <41451293+owahltinez@users.noreply.github.com>
2025-01-28 21:15:39 +1100

    Fix no-wrap typo

--- src/css_gen_utils.ts
+++ src/css_gen_utils.ts
@@ -110,7 +110,7 @@ const PROPS_CUSTOM = {
   "text-pretty": { "text-wrap": "pretty" },  
   // Whitespace.
   "whitespace-normal": { "white-space": "normal" },
-  "whitespace-no-wrap": { "white-space": "nowrap" },
+  "whitespace-nowrap": { "white-space": "nowrap" },
   "whitespace-pre": { "white-space": "pre" },
   "whitespace-pre-line": { "white-space": "pre-line" },
   "whitespace-pre-wrap": { "white-space": "pre-wrap" },
